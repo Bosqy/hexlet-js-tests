eedc780c16d4319ed824dd63f1195ef26429f6f9
Bosqy <groo@vth.ru>
2023-01-21 11:51:01 +1000

    add broken tests

--- src/capitalize.js
+++ src/capitalize.js
@@ -2,6 +2,9 @@ const capitalize = (text) => {
   if (text === '') {
     return '';
   }
+  if (text === undefined) {
+    return undefined;
+  };
   const firstChar = text[0].toUpperCase();
   const restSubstring = text.slice(1);
   return `${firstChar}${restSubstring}`;
